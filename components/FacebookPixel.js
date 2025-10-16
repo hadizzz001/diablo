a79b5760ab47fe627c9e42efe623135cc51fcabf
Ali Al Hadi Medlej <hadizzz001>
2025-10-16 14:48:46 +0300

    first commit

--- components/FacebookPixel.js
+++ components/FacebookPixel.js
@@ -1,35 +1,39 @@
-'use client';
-
-import Script from 'next/script';
+"use client";
+import { useEffect } from "react";
 
 export default function FacebookPixel() {
-  return (
-    <>
-      {/* Facebook Pixel Base Code */}
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1698473384307776'); 
-          fbq('track', 'PageView');
-        `}
-      </Script>
+  useEffect(() => {
+    // Load script
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = "2.0";
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, "script", "https://connect.facebook.net/en_US/fbevents.js");
 
-      {/* NoScript Fallback */}
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=1698473384307776&ev=PageView&noscript=1`}
-        />
-      </noscript>
-    </>
+    // Init Pixel
+    window.fbq("init", "1698473384307776");
+    window.fbq("track", "PageView");
+  }, []);
+
+  return (
+    <noscript>
+      <img
+        height="1"
+        width="1"
+        style={{ display: "none" }}
+        src="https://www.facebook.com/tr?id=1698473384307776&ev=PageView&noscript=1"
+      />
+    </noscript>
   );
 }
