775b5775b2151ad28c376d0996c07514694107be
Ali Al Hadi Medlej <hadizzz001>
2025-10-16 14:13:09 +0300

    first commit

--- components/FacebookPixel.js
+++ components/FacebookPixel.js
@@ -16,7 +16,7 @@ export default function FacebookPixel() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 'YOUR_PIXEL_ID'); 
+          fbq('init', '1698473384307776'); 
           fbq('track', 'PageView');
         `}
       </Script>
@@ -27,7 +27,7 @@ export default function FacebookPixel() {
           height="1"
           width="1"
           style={{ display: 'none' }}
-          src={`https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1`}
+          src={`https://www.facebook.com/tr?id=1698473384307776&ev=PageView&noscript=1`}
         />
       </noscript>
     </>
